c91584a9074a553cb8f38655dad16cc5a799b4f5
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-13 13:32:51 +0530

    Modified App Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { Container, Switch } from '@mui/material';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import SimpleBottomNavigation  from './components/MainNav';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+import Search from './Pages/Search/Search';
 
 function App() {
   return(
-    <><Header/>
-    <div className="app">
-     <SimpleBottomNavigation/>
-    </div>
-    </>
+    <BrowserRouter>
+    < Header/>
+      <div className="app">
+        Trending
+        <SimpleBottomNavigation/>
+         <Container>
+           <Switch>
+             <Route path='/' component= {Trending} exact />
+             <Route path='/movies' component= {Movies} />
+             <Route path='/series' component= {Series} />
+             <Route path='/search' component= {Search} />
+           </Switch>
+        </Container>
+      </div>
+
+     
+    </BrowserRouter>
   )
 }
 
