f01fecd9ddf402dfab9ac4f65b517cfd44e6a4a6
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-14 14:36:35 +0530

    Updated Trending files for axions

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,6 +1,11 @@
+import axios from "axios";
 
 
 const Trending  = () => {
+
+    // const fetchTrending = () => {
+    //     const {data} = await axios.get()
+    // }
     return (
         <div>
             <span className='pageTitle'>
