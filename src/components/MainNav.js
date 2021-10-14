f78f124f775034132a9c4a544184493c33c3cf93
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-14 14:35:47 +0530

    Bug fix

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -16,7 +16,7 @@ export default function SimpleBottomNavigation() {
     if(value === 0) history.push("/");
     else if(value === 1) history.push("/movies");
     else if(value === 2) history.push("/series");
-    else if(value == 3) history.push("/search");
+    else if(value === 3) history.push("/search");
   }, [value, history]);
 
   return (
