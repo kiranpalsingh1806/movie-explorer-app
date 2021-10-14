0afc79b3e2efca634f86c3c7417414d79a68bbf1
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-14 20:29:57 +0530

    Updated Trending page

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,16 +1,42 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+import Singlecontent from "../../components/SingleContent/SingleContent";
 
 
 const Trending  = () => {
 
-    // const fetchTrending = () => {
-    //     const {data} = await axios.get()
-    // }
+    const[content, setContent] = useState([]);
+
+    const fetchTrending = async () => {
+        const { data } = await axios.get(
+           `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+        );
+
+        setContent(data.results);
+    };
+
+    useEffect(() =>{
+        fetchTrending();
+    }, []);
+    
     return (
         <div>
             <span className='pageTitle'>
                 Trending
             </span>
+            <div className = "trending">
+                {
+                    content && content.map((c) => 
+                        <Singlecontent key={c.id}
+                        id={c.id}
+                        poster = {c.poster_path}
+                        title = {c.title || c.name}
+                        date={c.first_air_date || c.release_date}
+                        media_type = {c.media_type}
+                        vote_average = {c.vote_average}/>
+                    )
+                }
+            </div>
         </div>
     )
 }
