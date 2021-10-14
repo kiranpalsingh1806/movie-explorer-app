6393ed73c77ee07ff8c592c005144735aa98e3c4
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-14 20:29:13 +0530

    Added Single Content File

---
+++ src/components/SingleContent/SingleContent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Singlecontent = ({
+    id,
+    poster,
+    title,
+    date,
+    media_type,
+    vote_average,
+}) => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+}
+
+export default Singlecontent;
