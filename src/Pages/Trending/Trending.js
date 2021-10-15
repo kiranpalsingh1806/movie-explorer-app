e4ba263f36fb0a8efd0449224575a9e51e2a5127
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-15 09:53:58 +0530

    Updated Trending Page and Single Content file

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,19 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Singlecontent from "../../components/SingleContent/SingleContent";
-
+import './Trending.css';
 
 const Trending  = () => {
 
-    const[content, setContent] = useState([]);
+    const [content, setContent] = useState([])
 
     const fetchTrending = async () => {
         const { data } = await axios.get(
-           `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+           `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}`
         );
 
+        console.log(data);
         setContent(data.results);
     };
+
+    // useEffect(() => {
+    //     return () => {
+    //         fetchTrending();
+    //     };
+    // }, []); 
 
     useEffect(() =>{
         fetchTrending();
@@ -21,9 +28,7 @@ const Trending  = () => {
     
     return (
         <div>
-            <span className='pageTitle'>
-                Trending
-            </span>
+            <span className='pageTitle'>Trending</span>
             <div className = "trending">
                 {
                     content && content.map((c) => 
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,4 +1,7 @@
+import { Badge } from '@material-ui/core';
 import React from 'react';
+import { img_300, unavailable } from '../../config/config';
+import './SingleContent.css';
 
 const Singlecontent = ({
     id,
@@ -9,8 +12,18 @@ const Singlecontent = ({
     vote_average,
 }) => {
     return (
-        <div>
-            Hello
+        <div className="media">
+            <Badge badgeContent = {vote_average}
+            color = {vote_average > 6 ? 'primary':'secondary'}/>
+            <img
+            className="poster"
+            src={ poster ? `${img_300}/${poster}` : unavailable}
+            alt = {title} />
+            <b className = "title"> {title}</b>
+            <span className="subTitle">
+                {media_type === "tv" ? "TV Series" : "Movies"}
+                <span className= "subTitle">{date}</span>
+            </span>
         </div>
     );
 }
