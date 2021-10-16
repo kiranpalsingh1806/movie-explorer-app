0f6c3e88688d1868eaecbfc81c3e363cd084d13c
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-16 19:50:05 +0530

    Bug Fix

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,46 +1,69 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import MovieIcon from '@mui/icons-material/Movie';
-import SearchIcon from '@mui/icons-material/Search';
-import TvIcon from '@mui/icons-material/Tv';
-import { useHistory } from 'react-router';
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import TvIcon from "@material-ui/icons/Tv";
+import MovieIcon from "@material-ui/icons/Movie";
+import SearchIcon from "@material-ui/icons/Search";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    position: "fixed",
+    bottom: 0,
+    backgroundColor: "#2d313a",
+    zIndex: 100,
+  },
+});
 
 export default function SimpleBottomNavigation() {
+  const classes = useStyles();
   const [value, setValue] = React.useState(0);
   const history = useHistory();
-  
-  React.useEffect(() => {
-    if(value === 0) history.push("/");
-    else if(value === 1) history.push("/movies");
-    else if(value === 2) history.push("/series");
-    else if(value === 3) history.push("/search");
+
+  useEffect(() => {
+    if (value === 0) {
+      history.push("/");
+    } else if (value === 1) {
+      history.push("/movies");
+    } else if (value === 2) {
+      history.push("/series");
+    } else if (value === 3) {
+      history.push("/search");
+    }
   }, [value, history]);
 
   return (
-    <Box sx={{ 
-      width: '100%',
-      position: 'fixed',
-      bottom: 0,
-      backgroundColor: "#2d313a",
-      zIndex: 100,
-   }}>
-
-      <BottomNavigation
-         style={{backgroundColor: "#2d313a"}}
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction  style={{color: "white"}} label="Trending" icon={<WhatshotIcon />} />
-        <BottomNavigationAction  style={{color: "white"}} label="Movies" icon={<MovieIcon />} />
-        <BottomNavigationAction  style={{color: "white"}} label="TV Series" icon={<TvIcon />} />
-        <BottomNavigationAction  style={{color: "white"}} label="Search" icon={<SearchIcon />} />
-      </BottomNavigation>
-    </Box>
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Trending"
+        icon={<WhatshotIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Movies"
+        icon={<MovieIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="TV Series"
+        icon={<TvIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Search"
+        icon={<SearchIcon />}
+      />
+    </BottomNavigation>
   );
 }
--- src/components/Pagination/CustomPagination.js
+++ src/components/Pagination/CustomPagination.js
@@ -1,16 +1,38 @@
+import React from "react";
+import Pagination from "@material-ui/lab/Pagination";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
-import { Pagination } from '@mui/material';
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
-const Custompagination = () => {
-    return (
-        <div>
-            <Pagination/>
-        </div>
-    );
+export default function CustomPagination({ setPage, numOfPages = 10 }) {
+  // Scroll to top when page changes
+  const handlePageChange = (page) => {
+    setPage(page);
+    window.scroll(0, 0);
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 10,
+      }}
+    >
+      <ThemeProvider theme={darkTheme}>
+        <Pagination
+          onChange={(e) => handlePageChange(e.target.textContent)}
+          count={numOfPages}
+          color="primary"
+          hideNextButton
+          hidePrevButton
+        />
+      </ThemeProvider>
+    </div>
+  );
 }
-
-const styles = StyleSheet.create({})
-
-export default Custompagination;
