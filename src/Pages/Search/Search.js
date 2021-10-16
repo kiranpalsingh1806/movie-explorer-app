6ed555b411fa382e90ad9991f57fd997520074d0
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-16 17:06:18 +0530

    Updated fourth page Search

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,11 +1,40 @@
-
+import { Button, createMuiTheme, TextField } from "@material-ui/core";
 
 const Search  = () => {
+
+    const[type, setType] = useState(0);
+
+    const darkTheme = createMuiTheme({
+        palette: {
+            type: "dark",
+            primary: {
+                main:'#fff',
+            },
+        },
+    });
+
+
     return (
         <div>
-            <span className='pageTitle'>
-                Search
-            </span>
+            <ThemeProvider>
+                <div>
+            </div>
+            <TextField
+            style={{flex: 1}}
+            className = "searchBox"
+            label="Search"
+            variant="filled"
+            />
+
+            <Tabs value={type} indicatorColor = 'primary' textColor = 'primary'>
+                <Tab/>
+                <Tab/>
+            </Tabs>
+            </ThemeProvider>
+            <Button variant='contained' style={{marginLeft: 10}}>
+                <SearchIcon/>
+            </Button>
+
         </div>
     )
 }
